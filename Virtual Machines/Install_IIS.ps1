be8cf6b0a80b37ee7fef3adade4ca11db32d1855
shadabcloud <41741362+shadabcloud@users.noreply.github.com>
2018-10-25 11:58:31 +0530

    Update Install_IIS.ps1

--- Virtual Machines/Install_IIS.ps1
+++ Virtual Machines/Install_IIS.ps1
@@ -1,8 +1,7 @@
 # Install IIS
 $PublicSettings = '{"commandToExecute":"powershell Add-WindowsFeature Web-Server"}'
 
-Set-AzureRmVMExtension -ExtensionName "IIS" -ResourceGroupName $resourceGroup -VMName $vmName `
-  -Publisher "Microsoft.Compute" -ExtensionType "CustomScriptExtension" -TypeHandlerVersion 1.4 `
-  -SettingString $PublicSettings -Location $location
+Set-AzureRmVMExtension -ExtensionName "IIS" -Publisher "Microsoft.Compute" -ExtensionType "CustomScriptExtension" -TypeHandlerVersion 1.4 `
+-SettingString $PublicSettings
 
 
