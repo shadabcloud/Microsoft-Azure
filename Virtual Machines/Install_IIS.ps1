aff7053044f36d602a25bb6a37eb1eeb535ff596
shadabcloud <41741362+shadabcloud@users.noreply.github.com>
2018-11-03 00:52:33 +0530

    Delete Install_IIS.ps1

--- Virtual Machines/Install_IIS.ps1
+++
@@ -1,4 +0,0 @@
-Set-AzureRmVMExtension `
-  -ExtensionName IIS `
-  -VMName VM-APP-SIND `
-  -SettingString '{"commandToExecute":"powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"}'
